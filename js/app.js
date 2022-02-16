ac2521e023ab97dd8e76136599877c241382da63
azadk53 <mistazad@gmail.com>
2022-02-16 20:59:49 +0100

    responsive and saving commit

--- js/app.js
+++ js/app.js
@@ -1,31 +1,47 @@
+
 function getValue(id) {
     const idText = document.getElementById(id).value;
     const idValue = parseFloat(idText);
     return idValue;
 }
+/* calculate button event */
 document.getElementById('calculate').addEventListener('click', function () {
-    /* const incomeText = document.getElementById('income').value;
-    const income = parseFloat(incomeText); */
-    const income = getValue('income');
-    /*  const foodText = document.getElementById('food').value;
-     const food = parseFloat(foodText); */
+
+    var income = getValue('income');
+
     const food = getValue('food');
 
-    /*  const rentText = document.getElementById('rent').value;
-     const rent = parseFloat(rentText); */
     const rent = getValue('rent');
 
-    /*  const clothesText = document.getElementById('clothes').value;
-     const clothes = parseFloat(clothesText); */
-    const clothes = getValue('clothes')
+    const clothes = getValue('clothes');
 
+    /* expenses calculation */
     const expensesText = document.getElementById('expenses');
     const expenses = food + rent + clothes;
     expensesText.innerText = expenses;
 
+    /* balance calculation */
     const balanceText = document.getElementById('balance')
-    const balance = income - food - rent - clothes;
+    var balance = income - food - rent - clothes;
     balanceText.innerText = balance;
+})
+
+/* saving button event  */
+document.getElementById('btn-save').addEventListener('click', function () {
+
+    /* saving amount calculation */
+    const savePercent = document.getElementById('save').value;
+    var income = getValue('income');
+    const save = income * parseFloat(savePercent) / 100;
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = save;
+
+    /* remaining balance calculation */
+    const balanceText = document.getElementById('balance').innerText;
+    var balance = parseFloat(balanceText);
+    const remainingBalance = document.getElementById('remaining-balance');
+    const balanceCal = balance - save;
+    remainingBalance.innerText = balanceCal;
 
 
 })
